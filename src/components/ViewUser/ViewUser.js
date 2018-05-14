6a440d5b45e30e5fe4b915a2259247d0f9e4506b
Laszlo Fodor <LaszloFodor@users.noreply.github.com>
2018-05-14 22:34:38 +0200

    user info

--- src/components/ViewUser/ViewUser.js
+++ src/components/ViewUser/ViewUser.js
@@ -3,22 +3,20 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import fetch from 'isomorphic-fetch';
 
-import { Dialog } from 'components'
+import { Dialog } from 'components';
 
 class ViewUser extends Component {
-
   static propTypes = {
     // userId: PropTypes.number.isRequired,
     onEdit: PropTypes.func.isRequired,
     onDelete: PropTypes.func,
     onError: PropTypes.func,
     history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-    user: PropTypes.object.isRequired,  // eslint-disable-line react/forbid-prop-types
+    user: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
   };
 
-
   static defaultProps = {
-    onError: null,
+    onError: null
   };
 
   constructor() {
@@ -41,45 +39,57 @@ class ViewUser extends Component {
     fetch(`/api/user/myprofile`, {
       credentials: 'same-origin',
       method: 'DELETE',
-      headers,
-    }).
-      then((response) => {
+      headers
+    })
+      .then(response => {
         if (response.status < 200 || response.status >= 300) {
           throw new Error('Unable to retrieve user information!');
         }
         this.setState({ showConfirmDialog: false });
-        this.props.onDelete()
+        this.props.onDelete();
       })
-      .catch((error) => {
+      .catch(error => {
         window.console.log(error);
       });
   };
 
-  onDeleteButtonClick = (buttonId) => {
+  onDeleteButtonClick = buttonId => {
     if (buttonId === 'YES') {
       this.deleteUser();
     } else if (buttonId === 'NO') {
       this.setState({ showConfirmDialog: false });
     }
-  }
+  };
   render() {
     return (
       <div className="user-profile">
         <button onClick={this.props.onEdit}>Edit</button>
         <button onClick={this.onDeleteUserClick}>Delete</button>
-        {this.state.showConfirmDialog ? <Dialog title="Confirmation" message="Would you like to delete your profile?" onButtonClick={this.onDeleteButtonClick} /> : ''}
+        {this.state.showConfirmDialog ? (
+          <Dialog
+            title="Confirmation"
+            message="Would you like to delete your profile?"
+            onButtonClick={this.onDeleteButtonClick}
+          />
+        ) : (
+          ''
+        )}
         <div>
-          <p>sajt</p>
+          <div className="user-name">
+            <label htmlFor="name">Username: {this.props.user.username}</label>
+          </div>
+          <div className="user-firstname">
+            <label htmlFor="name">Firstname: {this.props.user.firstName}</label>
+          </div>
+          <div className="user-lastname">
+            <label htmlFor="name">Lastname: {this.props.user.lastName}</label>
+          </div>
+          <div className="user-email">
+            <label htmlFor="email">Email: {this.props.user.email}</label>
+          </div>
         </div>
       </div>
     );
-
   }
-
 }
 export default withRouter(ViewUser);
-
-
-
-
-
